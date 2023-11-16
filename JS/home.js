172294149d24ad7f8eda05df892d7021e402dabd
FatimaFatiMuni <Fatiqa780@gmail.com>
2023-11-16 17:11:07 +0500

    First commit

---
+++ JS/home.js
@@ -0,0 +1,42 @@
+let text = " ";
+function myFunction() {
+    text += 'typically comes with a 1.5-liter engine, offering a blend of performance and fuel efficiency. It features modern styling, a spacious interior, and advanced safety and infotainment technologies. Specifications can vary by model year and region.';
+    document.getElementById('text').innerHTML = text;
+}
+let wagon = " ";
+function wagonFunction() {
+    wagon += "The Wagon R is a compact car known for its space-efficient design, comfortable interior, and fuel efficiency. It offers practicality and versatility, making it a popular choice for urban driving and small families."
+    document.getElementById('wagon').innerHTML = wagon;
+}
+let civic = " ";
+function civicFunction() {
+    civic += "It offers a comfortable and spacious interior, making it a popular choice among drivers seeking a practical and economical vehicle.";
+    document.getElementById('civic').innerHTML = civic;
+}
+let alto = " ";
+function altoFunction() {
+    alto += "The Suzuki Alto is a compact car renowned for its fuel efficiency and agile maneuverability. It features a modest engine, user-friendly interior, and cost-effective maintenance, making it an ideal choice for urban commuting."
+    document.getElementById('alto').innerHTML = alto;
+}
+
+let audietron = " ";
+function audietronFunction() {
+    audietron += "and advanced driver-assistance features, it delivers a quiet, emissions-free driving experience while exemplifying Audi's commitment to eco-friendly mobility."
+    document.getElementById('audietron').innerHTML = audietron;
+}
+let bmw5 = " ";
+function bmw5Function() {
+    bmw5 += 'and comfortable interior make it a top choice for those seeking a premium driving experience. With a focus on both driving pleasure and practicality, the BMW 5 Series offers a dynamic and refined ride for discerning drivers.';
+    document.getElementById('bmw5').innerHTML = bmw5;
+}
+let tesla3 = " ";
+function teslaFunction() {
+    tesla3 += 'It offers a spacious interior, a range of up to 358 miles, and a minimalist dashboard with a large touchscreen for controlling various functions, making it a popular choice for eco-conscious drivers';
+    document.getElementById('tesla').innerHTML = tesla3;
+}
+
+let newcivic = " ";
+function newcivicFunction() {
+    newcivic += 'It comes in various trim levels and offers a range of engines, including hybrid options. A popular choice for those seeking practical transportation.';
+    document.getElementById('civic5').innerHTML = newcivic;
+}
---
+++ JS/showmore.js
@@ -0,0 +1,124 @@
+// audi section
+let text = " ";
+function myFunction() {
+    text += "The Audi A4 has 1 Petrol Engine on offer. The Petrol engine is 1998 cc . It is available with Automatic transmission. The A4 is a 5 seater 4 cylinder car and has length of 4762mm, width of 1847mm and a wheelbase of 2819mm.";
+    document.getElementById('text').innerHTML = text;
+}
+let texts = " ";
+function isfunction() {
+    texts += "The A7's standard powertrain is a turbocharged V-6 that makes 335 horsepower and 369 lb-ft of torque. That power is delivered to Audi's Quattro all-wheel drive through a seven-speed dual-clutch automatic transmission. The A7's standard V-6";
+    document.getElementById('texts').innerHTML = texts;
+}
+let font = " ";
+function hisfunction() {
+    font += "The Audi Q3 has 1 Petrol Engine on offer. The Petrol engine is 1984 cc . It is available with Automatic transmission.Depending upon the variant and fuel type the Q3 has a mileage of . The Q3 is a 5 seater 4 cylinder car.";
+    document.getElementById('font').innerHTML = font;
+}
+let fonts = " ";
+function thisfunction() {
+    fonts += "The Audi Q5 has 1 Petrol Engine on offer. The Petrol engine is 1984 cc . It is available with Automatic transmission.Depending upon the variant and fuel type the Q5 has a mileage of 13.47 kmpl . The Q5 is a 5 seater cylinder car.";
+    document.getElementById('fonts').innerHTML = fonts;
+}
+let click = " ";
+function clickfunction() {
+    click += "The Audi Q5 has 1 Petrol Engine on offer. The Petrol engine is 1984 cc . It is available with Automatic transmission.Depending upon the variant and fuel type the Q5 has a mileage of 13.47 kmpl . The Q5 is a 5 seater cylinder car.";
+    document.getElementById('all').innerHTML = click;
+}
+let type = " ";
+function typefunction() {
+    type += "The Q4 e-tron’s 77.0-kWh battery pack provides up to 265 miles per charge when paired with the standard single-motor 40 powertrain. The all-wheel drive 50 powertrain isn’t as efficient, with an estimated range of 236 miles for the squareback";
+    document.getElementById('on').innerHTML = type;
+}
+// ford section
+let maverick = " ";
+function maverickfunction() {
+    maverick += "Ford's minitruck returns for the 2024 model year. The Maverick is not only the brand's most affordable pickup, slotted below the Ranger and F-Series trucks, it's also the most financially accessible vehicle in the Blue Oval portfolio. ";
+    document.getElementById('maverick').innerHTML = maverick;
+}
+let ranger = " ";
+function rangerfunction() {
+    ranger += "Ford Ranger is available in 1 engine option(s) i.e. Diesel. The Ford Ranger is capable of generating a horsepower of 197 HP and a torque of 470 Nm with a top speed of 200 KM/H. Ranger is available in 1 transmission option(s) i.e Manual.";
+    document.getElementById('ranger').innerHTML = ranger;
+}
+let ford = " ";
+function fordfunction() {
+    ford += "The Ford Escape is a compact crossover SUV sold by Ford since the 2001 model year. The first Ford SUV derived from a car platform, the Escape was slotted below the Ford Explorer in size; the Escape is currently sized between the Ford ";
+    document.getElementById('ford').innerHTML = ford;
+}
+let mondeo = " ";
+function mondeofunction() {
+    mondeo += "The Ford Mondeo is a car manufactured by Ford since 1993. The first Ford model declared as a ,[4][5] the Mondeo was intended to consolidate several Ford model lines worldwide (the European Sierra, the Telstar in Asia";
+    document.getElementById('mondeo').innerHTML = mondeo;
+}
+let evos = " ";
+function evosfunction() {
+    evos += "Developed largely by a China-based team of Ford, the Evos has been described as a mix between a crossover and a fastback or station wagon due to its low-slung proportion.The interior of the Evos features a large touchscreen panel";
+    document.getElementById('evos').innerHTML = evos;
+}
+let everest = " ";
+function everestfunction() {
+    everest += "The third-generation Everest was revealed on 1 March 2022.[45][46] It was developed under the U704 development code, and known as the UB series in Australia.[47] Sharing most of the front end components with the P703 Ranger";
+    document.getElementById('everest').innerHTML = everest;
+}
+// suzuki section
+let swift = " ";
+function swiftfunction() {
+    swift += "The Suzuki Swift 2023 is a subcompact car manufactured by the Japanese automobile company Suzuki. It was first launched in September 2004 at the Paris Motor Show, and the first generation Swift was launched in 2005";
+    document.getElementById('swift').innerHTML = swift;
+}
+let wagon = " ";
+function wagonfunction() {
+    wagon += "Suzuki Wagon R is available in 1 engine option(s) i.e. Petrol. The Suzuki Wagon R is capable of generating a horsepower of 67 HP and a torque of 90 Nm with a top speed of 180 KM/H. Wagon R is available in Manual & Automatic.";
+    document.getElementById('wagon').innerHTML = wagon;
+}
+let ciaz = " ";
+function ciazfunction() {
+    ciaz += "The Suzuki Ciaz is generally powered by a fuel-efficient and reliable 4-cylinder engine with varying displacements and power outputs depending on the market. It offers good fuel economy it an excellent for daily commuting";
+    document.getElementById('ciaz').innerHTML = ciaz;
+}
+let cultus = " ";
+function cultusfunction() {
+    cultus += "New Cultus's low-to-ground tailgate opens wide, offering easy loading and unloading for everything you could possibly need. So head out with confidence, pride and power. The New Cultus offers more than enough luggage capacity ";
+    document.getElementById('cultus').innerHTML = cultus;
+}
+let alto = " ";
+function altofunction() {
+    alto += "The Alto 2023 comes with a new R06A engine which is more efficient and powerful. The new Alto has a 3-cylinder 660cc engine capable of producing up to 39&56 of torque. Moreover,Alto 2023 has a multipoint injection fuel distribution ";
+    document.getElementById('alto').innerHTML = alto;
+}
+let jimny = " ";
+function jimnyfunction() {
+    jimny += "The Jimny features a boxy and retro-inspired design that harks back to the original Jeep-like SUVs.Jimny is small but efficient engine, often a 1.5-liter 4-cylinder engine. It's designed for good fuel economy and decent performance its size.";
+    document.getElementById('jimny').innerHTML = jimny;
+}
+// Honda Section
+let civic = " ";
+function civicfunction() {
+    civic += "The Honda Civic 2023 is available in two different variants: Standard/Oriel and RS. The Standard/Oriel variant is equipped with a 1498cc DOHC dual VTC engine, delivering a maximum output of 95 kW at 5500-6000 rpm ";
+    document.getElementById('civic').innerHTML = civic;
+}
+let vezel = " ";
+function vezelfunction() {
+    vezel += "Honda Vezel is powered by a 4 cylinders DOHC 16 Valves Engine with an engine displacepemt of 1496 cc. The DOHC 16 Valves can boast the engine upto 160 hp @ 6600 RPM horsepower with 156 Nm @ 4600 RPM torque.";
+    document.getElementById('vezel').innerHTML = vezel;
+}
+let hrv = " ";
+function hrvfunction() {
+    hrv += "Honda HR-V 2023 is an international variant of Japanese Honda Vezel, a compact crossover without Hybrid technology. In Pakistan, Honda Atlas Cars Pakistan Limited launched the international second-generation ";
+    document.getElementById('hrv').innerHTML = hrv;
+}
+let brv = " ";
+function brvfunction() {
+    brv += "The Honda BRV has 1 Diesel Engine and 1 Petrol Engine on offer. The Diesel engine is 1498 cc while the Petrol engine is 1497 cc . It is available with Manual & Automatic transmission.the BRV has a mileage of 15.4-21.9 kmpl";
+    document.getElementById('brv').innerHTML = brv;
+}
+let city = " ";
+function cityfunction() {
+    city += "The Petrol engine is 1498 cc . It is available with Manual & Automatic transmission. Depending upon the variant and fuel type the City has a mileage of 17.8 to 18.4 kmpl. The City is a 5 seater 4 cylinder car and has length of 4583,";
+    document.getElementById('city').innerHTML = city;
+}
+let s660 = " ";
+function s660function() {
+    s660 += "Honda S660 is available in 1 engine option(s) i.e. Petrol. The Honda S660 is capable of generating a horsepower of 64 HP and a torque of 77 Nm with a top speed of 140 KM/H. S660 is available in 1 transmission";
+    document.getElementById('s660').innerHTML = s660;
+}
